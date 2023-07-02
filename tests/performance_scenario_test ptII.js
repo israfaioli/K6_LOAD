bf5c1a37f3f58808b3c36a78bd73adebd515c18e
Israel Faioli <68829837+israfaioli@users.noreply.github.com>
2023-07-02 11:16:40 -0300

    improve: add some test examples using different test types

---
+++ tests/performance_scenario_test ptII.js
@@ -0,0 +1,55 @@
+/*
+
+------------------- CENARIO DE TESTE -------------------
+Buscando todos os crocodilos
+
+Criterios aplicados:
+    - performance onde:
+        - carga de 100 vu por 10s
+
+Validações:
+    - tempo requisicao p(95) < 250ms
+    - requisicao com falha < 1%
+*/
+
+// IMPORTS
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 100,
+    duration: '10s',
+    thresholds: {
+        http_req_failed: ['rate < 0.01'],
+        http_req_duration: ['p(95) < 250']
+    }
+}
+
+const BASE_URL = 'https://test-api.k6.io';
+
+export function setup(){
+    const loginRes = http.post(`${BASE_URL}/auth/token/login/`, {
+        username: '0.5577608357004727@mail.com',
+        password: 'user123'
+    });
+    const token = loginRes.json('access');
+    return token;
+}
+
+
+export default function(token){
+    
+    const params = {
+        headers: {
+            Authorization: `Bearer ${token}` ,
+            'Content-Type': 'application/json'
+        }
+    }
+    const res = http.get(`${BASE_URL}/my/crocodiles`, params);
+
+    check(res, {
+        'status code 200': (r) => r.status === 200
+    });
+
+    sleep(1)
+}
---
+++ tests/performance_scenario_test.js
@@ -0,0 +1,53 @@
+/*
+
+------------------- CENARIO DE TESTE -------------------
+Realizar registro de um novo usuario
+
+Criterios aplicados:
+    - performance onde:
+        - carga de 10 vu por 10s
+
+Validações:
+    - requisicao com sucesso > 95%    
+    - tempo requisicao p(95) < 500ms
+    - requisicao com falha < 1%
+*/
+
+import http from "k6/http";
+import { check, sleep} from "k6"; 
+
+// sleep faz com que cada vu (virtual user) defina um tempo para que ela realize uma próxima requisicao
+
+
+export const options = {
+    stages: [
+        {duration: '10s', target: 10},
+    ],
+    thresholds: {
+        http_req_failed: ['rate < 0.01'],
+        http_req_duration: ['p(95) < 500'],
+        checks: ['rate > 0.95']
+    }
+}
+
+export default function () {
+    const USER = `${Math.random()}@mail.com`
+    const PASS = 'user123'
+    const BASE_URL = 'https://test-api.k6.io';
+
+    console.log( USER + PASS);
+
+    const res = http.post(`${BASE_URL}/user/register/`, {
+        username: USER,
+        first_name: 'crocrodilo',
+        last_name: 'dino',
+        email: USER,
+        password: PASS
+    });
+
+    check(res, {
+        'sucesso ao registar': (r) => r.status === 201
+    });
+
+    sleep(1)
+}
